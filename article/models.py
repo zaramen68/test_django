86f6c2653946387a510f1872420d08ad5793f861
zaram <zaramenskiy@awada.ru>
2021-04-29 12:53:36 +0300

    last

--- article/models.py
+++ article/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
 
 class Author(models.Model):
-  name = models.CharField(max_length=255)
-  email = models.EmailField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+      return self.name
 
 
 
 class Article(models.Model):
-  title = models.CharField(max_length=120)
-  description = models.TextField()
-  body = models.TextField()
-  author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+      return self.title
 
 class Question(models.Model):
     TEXT='T'
@@ -37,10 +37,6 @@ class Question(models.Model):
     )
     title = models.CharField(max_length=120)
     description = models.TextField()
-    # polls = models.OneToOneField(Poll, on_delete = models.SET_NULL, null=True)
-    # bodyJson = models.JSONField(null=True)
-    # polls = models.ManyToManyField(Poll)
-    # polls = models.ForeignKey('Poll', related_name='questions', on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.description
 
@@ -59,11 +55,6 @@ class Choice(models.Model):
     poll = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-    def votes_percentage(self):
-        """Calculates the percentage of votes for this choice."""
-        total = self.poll.total_votes()
-        return self.votes / float(total) * 100 if total > 0 else 0
 
     def __unicode__(self):
         """Returns a string representation of a choice."""
